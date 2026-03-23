191a3126f505365b533e04b7058e2c64a3ffcd0d
Jay7Code <arjayvelasco09@gmail.com>
2026-03-23 12:58:25 +0800

    modified the PHPMAILER path

--- email/config.php
+++ email/config.php
@@ -10,10 +10,10 @@
 define('SMTP_HOST', 'cpanel10wh.jpt1.cloud.z.com');
 
 // Your SMTP Username (Email Address)
-define('SMTP_USERNAME', 'noreply.johnhayhotels@theforestwing.com');
+define('SMTP_USERNAME', 'feedback@theforestwing.com');
 
 // Your SMTP Password or App Password
-define('SMTP_PASSWORD', 'ENTER_YOUR_PASSWORD_HERE');
+define('SMTP_PASSWORD', 'pt^X[j*PE{kt3cy!'); // <-- PLEASE ENTER YOUR ACTUAL PASSWORD HERE
 
 // Your SMTP Port (465 for SSL, 587 for TLS)
 define('SMTP_PORT', 465);
